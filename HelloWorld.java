cbc70b8ef0b61d89f6f03ba277829c0091a0bd17
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-04-30 15:40:00 -0300

    chore: data type

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        
+        /*
+         * Números (int, double, float, long)
+         * Textos (String)
+         * Boolean (boolean)
+         */
+
+         int dadoDoTipoInt = 10;
+         double dadoDoTipoDouble = 3.14;
+         float dadoDoTipoFloat = 3.14f;
+         long dadoDoTipoLong = 872787287879819818L;
+         String dadoDoTipoString = "Colocar o meu texto";
+         boolean dadoDoTipoBoolean = false;
     }
 }
