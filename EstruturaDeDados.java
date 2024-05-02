2f223fb5bcf789b381ab6593d66b95463a9a9f78
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-05-02 18:28:25 -0300

    chore: list data structure

---
+++ EstruturaDeDados.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstruturaDeDados {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Vinícius");
+        nomes.add("Pedro");
+        nomes.add("Luan");
+        nomes.add("Caio");
+
+        System.out.println(nomes.get(0));
+
+        // for (String nome : nomes) {
+        //     System.out.println("O nome é " + nome);
+        // }
+
+        nomes.forEach(nome -> System.out.println("O nome é " + nome));
+    }
+}
