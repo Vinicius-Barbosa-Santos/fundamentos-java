2de6df047074e325e72023d578e09c99c8385715
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-05-08 13:29:27 -0300

    chore: constructor

---
+++ construtor/TesteDeConstrutor.java
@@ -0,0 +1,8 @@
+package construtor;
+
+public class TesteDeConstrutor {
+    public static void main(String[] args) {
+        Construtor construtor = new Construtor(10, "Vinicius");     
+        Construtor construtor1 = new Construtor(2);
+    }
+}
